031f1fe8a750e4eb5a6d493fee95e9c851b3c8e3
CatalinA <catalin.popa@isa.utm.md>
2023-02-26 00:58:14 +0200

    Shop page, Sport page, Event Page

--- EasyBreath/EasyBreath.web/Registration.aspx.cs
+++ EasyBreath/EasyBreath.web/Registration.aspx.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿
+using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
-namespace EasyBreath.web
-{
-     public partial class Registration : System.Web.UI.Page
-     {
-          protected void Page_Load(object sender, EventArgs e)
-          {
-
-          }
-     }
-}
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Reflection.Emit;
