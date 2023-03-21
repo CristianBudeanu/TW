5806e66c70d121e3bce86b5fb8f184cda7c20107
Cristian Budeanu <budeanu.cristian28@gmail.com>
2023-03-21 21:36:44 +0200

    small changes

---
+++ EasyBreath/EasyBreath.Domain/Entities/User/UCookieData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.User
+{
+     public class UCookieData
+     {
+          public string UserName { get; set; }
+          public string Token { get; set; }    
+     }
+
+}
