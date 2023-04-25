d9ac0a0e8d74826256e0bd23f9aacae3b311380a
Cristian Budeanu <budeanu.cristian28@gmail.com>
2023-04-25 19:52:19 +0300

    Add USER control

--- EasyBreath/EasyBreath.web/ActionAtributes/MyActionFilter.cs
+++ EasyBreath/EasyBreath.web/ActionAtributes/MyActionFilter.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace EasyBreath.web.AtionAtributes
 {
-     public class MyAcctionFilter
+     public class MyActionFilter : ActionFilterAttribute
      {
+          public override void OnActionExecuting(ActionExecutingContext filterContext)
+          {
+               
+          }
+
+          public override void OnActionExecuted(ActionExecutedContext filterContext)
+          {
+               
+          }
      }
 }
