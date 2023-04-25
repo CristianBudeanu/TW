2200b14185710cfce0c10e3c2e39972fa4fff07c
CatalinA <catalin.popa@isa.utm.md>
2023-04-25 19:46:59 +0300

    Added Domain

---
+++ EasyBreath/EasyBreath.Domain/Entities/User/UChangePasswordData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyBreath.Domain.Entities.User
+{
+     public class UChangePasswordData
+     {
+          public string NewPassword { get; set; }
+          public string ConfirmedPassword { get; set; }
+     }
+}
